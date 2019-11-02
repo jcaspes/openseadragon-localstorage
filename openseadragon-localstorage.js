714aee3ec6620f77e0b45a0984f33a517ebfbdac
jc <jc@skadubpc.net>
2019-11-02 21:41:58 +0100

    enhance storage key to have parameters stored for each viewer

--- openseadragon-localstorage.js
+++ openseadragon-localstorage.js
@@ -10,31 +10,46 @@
             throw new Error('OpenSeadragon is missing.');
         }
     }
-
+	
+	function storageGetItem(key)
+	{
+		if(window.localStorage && window.localStorage.hasOwnProperty(key))
+		{
+			return window.localStorage.getItem(key);
+		}
+		return null;
+	}
+	
+	function storageGetFloatItem(viewerId, key, defaultValue)
+	{
+		var computedKey = 'openseadragon-' + viewerId + '-' + key;
+		var value = storageGetItem(computedKey);
+		if(value != null && !isNaN(value))
+		{
+			return parseFloat(value);
+		}
+		return defaultValue;
+	}
+	
+	function storageSetItem(viewerId, key, value)
+	{
+		var computedKey = 'openseadragon-' + viewerId + '-' + key;
+		if(window.localStorage)
+		{
+			window.localStorage.setItem(computedKey, value);
+		}
+	}
     // ----------
     $.Viewer.prototype.localStorage = function() {
         var self = this;
 		
 		// Read storage infos to inint view on first use
 		var params = {
-			zoom:1,
+			zoom: storageGetFloatItem(self.id, 'zoom', 1.0),
 			pan : {
-				x: 0.5,
-				y: 0.5
+				x: storageGetFloatItem(self.id, 'pan.x', 0.5),
+				y: storageGetFloatItem(self.id, 'pan.y', 0.5),
 		}};
-		
-		if(window.localStorage.hasOwnProperty('openseadragon-zoom'))
-		{
-			params.zoom = parseFloat(window.localStorage.getItem('openseadragon-zoom'));
-		}
-		if(window.localStorage.hasOwnProperty('openseadragon-pan.x'))
-		{
-			params.pan.x = parseFloat(window.localStorage.getItem('openseadragon-pan.x'));
-		}
-		if(window.localStorage.hasOwnProperty('openseadragon-pan.y'))
-		{
-			params.pan.y = parseFloat(window.localStorage.getItem('openseadragon-pan.y'));
-		}
 
 		// Avoid openseadragon internal updates during start to trigger an update event
 		var ignoreUpdates = true;
@@ -43,11 +58,11 @@
             var zoom = self.viewport.getZoom();
             var pan = self.viewport.getCenter();
 			
-            if (paramsToUse.zoom !== undefined && paramsToUse.zoom !== zoom) {
+            if (paramsToUse.zoom !== zoom) {
                 self.viewport.zoomTo(paramsToUse.zoom, null, true);
             }
 
-            if (paramsToUse.pan.x !== undefined && paramsToUse.pan.y !== undefined && (paramsToUse.pan.x !== pan.x || paramsToUse.pan.y !== pan.y)) {
+            if (paramsToUse.pan.x !== pan.x || paramsToUse.pan.y !== pan.y) {
                 self.viewport.panTo(new $.Point(paramsToUse.pan.x, paramsToUse.pan.y), true);
             }
 			ignoreUpdates = false;
@@ -69,9 +84,9 @@
 			{
 				var zoom = self.viewport.getZoom();
 				var pan = self.viewport.getCenter();
-				window.localStorage.setItem('openseadragon-zoom', zoom);
-				window.localStorage.setItem('openseadragon-pan.x', pan.x);
-				window.localStorage.setItem('openseadragon-pan.y', pan.y);
+				storageSetItem(self.id, 'zoom', zoom);
+				storageSetItem(self.id, 'pan.x', pan.x);
+				storageSetItem(self.id, 'pan.y', pan.y);
 			}
         };
 		
